ba9926f82556d3935ad6e8ecee1eba792965cb15
AD-Codex <126350818+AD-Codex@users.noreply.github.com>
2023-04-17 17:39:34 +0530

    Delete MQTT_files directory

--- MQTT_files/Python_MQTT/mqtt_publish_demo.py
+++
@@ -1,8 +0,0 @@
-#MQTT publish demo
-#publish two messages to 2 different topics
-
-import paho.mqtt.publish as publish
-
-publish.single("eeuop/SS/status/H","Test1",hostname = "test.mosquitto.org")
-publish.single("eeuop/SS/status/W","Test2",hostname = "test.mosquitto.org")
-print("Done")
